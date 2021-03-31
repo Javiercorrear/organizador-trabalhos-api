60ebd7d930e9104c4d2d3ae278170813e97b6844
Javier Correa <javier.correa@taglivros.com.br>
2021-03-31 10:13:44 -0300

    corrigir problemas de estilo.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-const express = require('express')
+require( 'dotenv' ).config()
+const express = require( 'express' )
 const cors = require( 'cors' )
 const { json, urlencoded } = require( 'body-parser' )
 
-const loginApi = require( './api/login' )
+const userApi = require( './api/user' )
 
 const port = process.env.PORT || 80
 const app = express()
@@ -12,15 +12,15 @@ app.use( json() )
 app.use( urlencoded( { extended: true } ) )
 app.use( cors() )
 
-app.post( '/user', loginApi.createUser )
+app.post( '/user', userApi.createUser )
 
-app.get('/', (req, res) => {
+app.get( '/', ( req, res ) => {
     res.send( {
         system: 'Organizador de trabalhos acadêmicos',
         team: 'Javier Correa, Kerollyn, Thiago Lacerda'
     } )
-})
+} )
 
-app.listen(port, function() {
-    console.log(`App rodando em http://localhost:${ port }`)
-})
+app.listen( port, function() {
+    console.log( `App rodando em http://localhost:${ port }` )
+} )
