a79cef00a23d667974919262b33682ed418ba1dd
javiercorrear <javiercorrear@gmail.com>
2021-04-03 20:58:03 -0300

    incluir teste para busca de usuário no data layer.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # organizador-trabalhos-api
 Api para o sistema gerenciador de trabalhos acadêmicos desenvolvido para o projeto da disciplina de laboratório de software e projetos.
 
+## Requisitos
+Para poder publicar a aplicação é necessário instalar o *heroku CLI*, salvo para ambiente local.
 ## Executar servidor local
 Para rodar o servidor localmente, execute o seguinte comando:
 ````
@@ -11,6 +13,12 @@ npm start
 Para hospedar nossa API optamos por utilizar o heroku. O servidor roda em:
 ````
 https://heroku-org-trabalhos-api.herokuapp.com
+````
+
+## Testes
+Para rodar os testes, execute o seguinte comando:
+````
+npm test
 ````
 
 ## Deploy
--- __tests__/data-layer/user.test.js
+++ __tests__/data-layer/user.test.js
@@ -2,7 +2,6 @@ jest.mock( '../../src/lib/mongodbApi' )
 const mongoApi = require( '../../src/lib/mongodbApi' )
 const userDataLayer = require( '../../src/data-layer/user' )
 const bcrypt = require( 'bcrypt' )
-const User = require( '../../src/model/User' )
 
 const USER_COLLECTION = 'User'
 const SALT_ROUNDS = 10
@@ -11,12 +10,20 @@ const MOCK_USER_NAME = 'user_name'
 const MOCK_PASSWORD = 'some_password'
 
 describe( 'Testing user data layer', () => {
-    test( 'test 1', async() => {
+    test( 'funçao createUser deve chamar as funçoes com os parametros corretos.', async() => {
         const hashSpy = jest.spyOn( bcrypt, 'hash' )
         const insertSpy = jest.spyOn( mongoApi, 'insertOne' )
         await userDataLayer.createUser( MOCK_USER_NAME, MOCK_PASSWORD )
 
         expect( hashSpy ).toBeCalledWith( MOCK_PASSWORD, SALT_ROUNDS )
         expect( insertSpy ).toBeCalled()
+    } )
+
+    test( 'Função findUserByUserName deve chamar as funçoes com os parametros corretos.', async() => {
+        const findOneSpy = jest.spyOn( mongoApi, 'findOne' )
+        await userDataLayer.findUserByUserName( MOCK_USER_NAME )
+
+        const expectedQuery = { userName: MOCK_USER_NAME }
+        expect( findOneSpy ).toBeCalledWith( { query: expectedQuery, collectionName: USER_COLLECTION } )
     } )
 } )
