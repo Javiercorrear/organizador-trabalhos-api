383b45a42faf82af02c88002aad2a670c5e2c0e6
Javier Correa <javier.correa@taglivros.com.br>
2021-04-09 00:33:56 -0300

    adicionar campo com nome do arquivo ao modelo Media.

--- src/data-layer/fileHandler.js
+++ src/data-layer/fileHandler.js
@@ -6,9 +6,10 @@ const Media = require( '../model/Media' )
 const MEDIA_COLLECTION = 'Media'
 
 const fileUpload = async( file, userId, description ) => {
-    file.originalname = `${ uuidV1() }_${ file.originalname }`
+    const { originalname } = file
+    file.originalname = `${ uuidV1() }_${ originalname }`
     const fileUrl = await gcsApi.uploadImage( file )
-    const media = new Media( { userId, url: fileUrl, description } )
+    const media = new Media( { userId, fileName: originalname, url: fileUrl, description } )
     const insertedMedia = await mongoApi.insertOne( { document: media, collectionName: MEDIA_COLLECTION } )
     return Media.getFormattedMedia( insertedMedia )
 }
--- src/model/Media.js
+++ src/model/Media.js
@@ -1,17 +1,19 @@
 const { v1: uuidV1 } = require( 'uuid' )
 
 module.exports = class Media {
-    constructor( { _id, userId, url, description } ){
+    constructor( { _id, userId, fileName, url, description } ){
         this._id = _id || uuidV1()
         this.userId = userId
+        this.fileName = fileName
         this.url = url || null
         this.description = description || null
     }
 
-    static getFormattedMedia( { _id, userId, url, description } ) {
+    static getFormattedMedia( { _id, userId, fileName, url, description } ) {
         return {
             id: _id,
             userId,
+            fileName,
             url,
             description
         }
