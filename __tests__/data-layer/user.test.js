b52af64bef816df448ca3d873d79ee593754da3d
Javier Correa <javier.correa@taglivros.com.br>
2021-04-16 21:58:19 -0300

    corrigir testes.

--- __tests__/data-layer/user.test.js
+++ __tests__/data-layer/user.test.js
@@ -1,6 +1,6 @@
+const bcrypt = require( 'bcrypt' )
 const mongoApi = require( '../../src/lib/mongodb-api/mongodbApi' )
 const userDataLayer = require( '../../src/data-layer/user' )
-const bcrypt = require( 'bcrypt' )
 
 const USER_COLLECTION = 'User'
 const SALT_ROUNDS = 10
@@ -13,7 +13,7 @@ describe( 'Testing user data layer', () => {
     test( 'funçao createUser deve chamar as funçoes com os parametros corretos.', async() => {
         const hashSpy = jest.spyOn( bcrypt, 'hash' )
         const insertSpy = jest.spyOn( mongoApi, 'insertOne' )
-        await userDataLayer.createUser( MOCK_USER_NAME, MOCK_EMAIL, MOCK_PASSWORD )
+        await userDataLayer.createUser( MOCK_USER_NAME, MOCK_EMAIL, MOCK_PASSWORD, MOCK_PASSWORD )
 
         expect( hashSpy ).toBeCalledWith( MOCK_PASSWORD, SALT_ROUNDS )
         expect( insertSpy ).toBeCalled()
