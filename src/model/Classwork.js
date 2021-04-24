79bc6d2e6398dc3533dd33f46d0fcf256815307a
Javier Correa <javiercorrear@gmail.com>
2021-04-24 02:28:06 -0300

    fix query projection.

--- src/model/Classwork.js
+++ src/model/Classwork.js
@@ -2,13 +2,15 @@ const { v1: uuidV1 } = require( 'uuid' )
 const statusEnum = require( '../shared/classWorkStatusEnum' )
 
 module.exports = class Classwork {
-    constructor( { _id, userId, fileName, url, title, subject, professorName, description, status, createdAt, updatedAt } ){
+    constructor( {
+        _id, userId, fileName, url, title, subject, professorName, description, status, createdAt, updatedAt
+    } ){
         const now = new Date()
 
         this._id = _id || uuidV1()
         this.userId = userId
         this.title = title
-        this.subject = subject,
+        this.subject = subject
         this.professorName = professorName
         this.fileName = fileName
         this.url = url || null
@@ -18,7 +20,9 @@ module.exports = class Classwork {
         this.updatedAt = updatedAt || now
     }
 
-    static getFormattedClasswork( { _id, userId, fileName, url, title, subject, professorName, description, status, createdAt, updatedAt } ) {
+    static getFormattedClasswork( {
+        _id, userId, fileName, url, title, subject, professorName, description, status, createdAt, updatedAt
+    } ) {
         return {
             id: _id,
             userId,
@@ -37,7 +41,7 @@ module.exports = class Classwork {
     static getProjection() {
         return {
             _id: 0,
-            id: '_id',
+            id: '$_id',
             userId: 1,
             fileName: 1,
             url: 1,
